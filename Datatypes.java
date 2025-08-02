151d1395d1b2a2ee7dcd30c9c38e8141a16b5c37
Altraix <altraix@localhost.localdomain>
2025-08-02 18:00:20 +0530

    Add Dataypes.java (2025-08-02)

---
+++ Datatypes.java
@@ -0,0 +1,21 @@
+public class Datatypes{
+    public static void main(String[] args) {
+        byte a = 10;
+        short b = 2000;
+        int c = 300000;
+        long d = 4000000000L;  // Notice the 'L' suffix
+        float e = 5.75f;       // Notice the 'f' suffix
+        double f = 19.99;
+        char g = 'A';
+        boolean isJavaFun = true;
+
+        System.out.println("byte: " + a);
+        System.out.println("short: " + b);
+        System.out.println("int: " + c);
+        System.out.println("long: " + d);
+        System.out.println("float: " + e);
+        System.out.println("double: " + f);
+        System.out.println("char: " + g);
+        System.out.println("boolean: " + isJavaFun);
+    }
+}

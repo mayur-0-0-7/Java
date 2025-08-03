d1776a1a3141da0fc543f6907a1a86bb2abcc6da
Altraix <altraix@localhost.localdomain>
2025-08-03 14:45:55 +0530

    control flow using if else

---
+++ ifelse.java
@@ -0,0 +1,17 @@
+public class ifelse {
+
+    public static void main(String[] args) {
+        
+        int age = 20;
+    
+        if(age >=18 ){
+        System.out.println("your are an adult");
+
+    }
+    else{
+        System.out.println("you are a minor");
+    }
+    }
+  
+    
+}
